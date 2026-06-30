64458ec111dce27d333bdcd9af973c5b9b3c65f0
Jeanjx16Z <jeanjeasen@gmail.com>
2026-07-01 02:16:40 +0700

    Add logger setup and test logging messages in test_logger.py

---
+++ test/unit/test_logger.py
@@ -0,0 +1,9 @@
+from app.utils.logger import setup_logger
+
+logger = setup_logger("RecruitmentAgent")
+
+logger.info("Application Started")
+
+logger.warning("Testing Warning")
+
+logger.error("Testing Error")
